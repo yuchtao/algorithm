796ed8e29aa37860990357fad4266b1f9f03c4b6
ke <ke>
2024-01-07 00:56:16 +0800

    测试提交

---
+++ src/main/java/leetcode/leetcode/editor/cn/[29]两数相除.java
@@ -0,0 +1,21 @@
+package leetcode.leetcode.editor.cn;
+
+/**
+ * 两数相除
+ * 2024-01-07 00:49:10
+ */
+class DivideTwoIntegers {
+    public static void main(String[] args) {
+        Solution solution = new DivideTwoIntegers().new Solution();
+    }
+    
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int divide(int dividend, int divisor) {
+        return 0;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+    
+}
